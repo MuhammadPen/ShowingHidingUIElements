789899971dae4f2af42ea9bc048823ff55443a3e
MuhammadPEN <Splasheoo@gmail.com>
2017-12-19 15:17:03 +0500

    die

---
+++ app/src/main/java/com/muhammadpen/showinghidinguielements/MainActivity.java
@@ -0,0 +1,26 @@
+package com.muhammadpen.showinghidinguielements;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView die;
+
+    public void showButtonPress (View view) {
+        die.setVisibility(View.VISIBLE);
+    }
+    public void hideButtonPress (View view) {
+        die.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        die = (TextView) findViewById(R.id.dieTextView);
+    }
+}
